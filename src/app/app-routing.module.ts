b6faf0b05f3bc88d321b506cbb9caa1e8abc9c67
Link <73287503+vubaolinh123@users.noreply.github.com>
2022-05-10 08:56:21 +0700

    update bla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HelloComponent } from './components/hello/hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "hello", component: HelloComponent},
+  {path: "about", component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './components/hello/hello.component';
+import { AboutComponent } from  './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
